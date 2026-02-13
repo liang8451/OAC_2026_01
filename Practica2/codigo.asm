78be58e68ab889aa9386b3081d2328bc030b5dcc
liang8451 <124332600+liang8451@users.noreply.github.com>
2026-02-13 11:13:52 -0800

    Update codigo.asm

--- Practica2/codigo.asm
+++ Practica2/codigo.asm
@@ -1,39 +1,13 @@
 %include "./pc_io.inc"
 global _start
-section .text
-_start:
-
-	;INSTRUCCION 3
-	;sys_write(stdout, message, length)
-	;mov eax, 4 
-	;mov ebx, 1 
-	;mov ecx, msj 
-	;mov edx, len 
-	;int 80h 
-
-	;mov eax, 3 
-	;mov ebx, 0 
-	;mov ecx, num1 
-	;mov edx, 1 
-	;int 80h 
-
-	;sys_write(stdout, message, length)
-	;mov eax, 4 
-	;mov ebx, 1 
-	;mov ecx, num1 
-	;mov edx, 1 
-	;int 80h 
-
-	;INSTRUCCION 4
 
 section .data
 
 	; INSTRUCCION 1
 	;Esto define una cadena de texto con salto de línea (0x0A) y una constante len que guarda la longitud del mensaje.
 
-	msj: db 'Ingrese un dígito (0-9)',0x0  ; mensaje y nueva linea
-	len: equ $-msj							
-
+	msj: db 'Ingrese un dígito (0-9)',0x0A  ; mensaje y nueva linea
+	len: equ $-msj	
 
 section .bss
 
@@ -41,3 +15,39 @@ section .bss
 	;Aquí se reserva un byte para almacenar el valor ingresado.
 
 	num1 resb 1 
+
+section .text
+_start:
+
+	;INSTRUCCION 3
+
+	;sys_write(stdout, message, length)
+	mov eax, 4 
+	mov ebx, 1 
+	mov ecx, msj 
+	mov edx, len 
+	int 80h 
+
+	mov eax, 3 
+	mov ebx, 0 
+	mov ecx, num1 
+	mov edx, 1 
+	int 80h 
+
+	;sys_write(stdout, message, length)
+	mov eax, 4 
+	mov ebx, 1 
+	mov ecx, num1 
+	mov edx, 1 
+	int 80h 
+
+	mov eax 1
+	mov ebx 0
+	int 80h
+
+	;INSTRUCCION 4
+
+						
+
+
+
