557dc37054bfa8232200e6792219e1296fd422a9
liang8451 <124332600+liang8451@users.noreply.github.com>
2026-02-14 20:06:30 -0800

    Update codigo.asm

--- Practica2/codigo.asm
+++ Practica2/codigo.asm
@@ -6,7 +6,7 @@ section .data
 	; INSTRUCCION 1
 	;Esto define una cadena de texto con salto de línea (0x0A) y una constante len que guarda la longitud del mensaje.
 
-	msj: db 'Ingrese un dígito (0-9)',0x0A  ; mensaje y nueva linea
+	msj: db 'Ingrese un dígito (0-9)',0x0  ; mensaje 
 	len: equ $-msj	
 
 section .bss
@@ -22,32 +22,61 @@ _start:
 	;INSTRUCCION 3
 
 	;sys_write(stdout, message, length)
-	mov eax, 4 
-	mov ebx, 1 
-	mov ecx, msj 
-	mov edx, len 
-	int 80h 
+	;mov eax, 4 
+	;mov ebx, 1 
+	;mov ecx, msj 
+	;mov edx, len 
+	;int 80h 
 
-	mov eax, 3 
-	mov ebx, 0 
-	mov ecx, num1 
-	mov edx, 1 
-	int 80h 
+	;mov eax, 3 
+	;mov ebx, 0 
+	;mov ecx, num1 
+	;mov edx, 1 
+	;int 80h 
 
 	;sys_write(stdout, message, length)
-	mov eax, 4 
-	mov ebx, 1 
-	mov ecx, num1 
-	mov edx, 1 
-	int 80h 
+	;mov eax, 4 
+	;mov ebx, 1 
+	;mov ecx, num1 
+	;mov edx, 1 
+	;int 80h 
 
-	mov eax 1
-	mov ebx 0
+	;INSTRUCCION 5
+
+	;mov edx, msj
+	;call puts
+
+	;call getch
+
+	;call putchar
+
+	;INSTRUCCION 7
+
+	mov edx, msj 
+	call puts 
+	call salto 
+	call getch 
+	call salto 
+	call putchar 
+	call salto
+
+	;INSTRUCCION 9
+
+	mov ebx, num1 
+	mov byte[ebx], al 
+	add byte[ebx], 1 
+	mov al, byte[ebx] 
+	call putchar 
+
+	mov eax, 1
+	mov ebx, 0
 	int 80h
 
-	;INSTRUCCION 4
-
-						
-
-
-
+	salto: 
+		pushad 
+		mov al, 13 
+		call putchar 
+		mov al, 10 
+		call putchar 
+		popad 
+		ret 
